dd0fd47bc10a65a482085f18c2973c0aa05d9f3b
Diegoapulgarin <dapulgaris@eafit.edu.co>
2023-03-14 11:56:47 -0500

    minor changes

--- cGAN_subsampling/cGAN_subsampling.py
+++ cGAN_subsampling/cGAN_subsampling.py
@@ -42,6 +42,7 @@ import numpy as np
 
 import tensorflow as tf
 import tensorflow.image as timg
+import tensorflow.keras.backend as K
 #%% custom loss function
 
 def ssim_loss(y_true, y_pred):
@@ -56,8 +57,13 @@ def ssim_loss(y_true, y_pred):
 
 def power_spectrum_loss(y_true, y_pred):
     # Compute the 2D Fourier transform of the reference and predicted images
-    # y_true = y_true[:,:,:,0]+1j*y_true[:,:,:,1]
-    # y_pred = y_pred[:,:,:,0]+1j*y_pred[:,:,:,1]
+    # y_pred = tf.squeeze(y_pred)
+    y_true = tf.transpose(y_true, perm=[1, 2, 0,1])
+    y_pred = tf.transpose(y_pred, perm=[1, 2, 0,1])
+    y_true = tf.cast(y_true, tf.complex64)
+    y_pred = tf.cast(y_pred, tf.complex64)
+    y_true = y_true[:,:,:,0]+1j*y_true[:,:,:,1]
+    y_pred = y_pred[:,:,:,0]+1j*y_pred[:,:,:,1]
     ref_fft = tf.signal.fft2d(tf.cast(y_true, tf.complex64))
     pred_fft = tf.signal.fft2d(tf.cast(y_pred, tf.complex64))
     
@@ -215,8 +221,8 @@ def define_gan(g_model, d_model, image_shape):
     # src image as input, generated image and classification output
     model = Model(in_src, [dis_out, gen_out])
     # compile model
-    opt = Adam(lr=0.0002, beta_1=0.5)
-    model.compile(loss=power_spectrum_loss, optimizer=opt, loss_weights=None) # =['mean_squared_error', 'mae']
+    opt = Adam(learning_rate=0.0002, beta_1=0.5)
+    model.compile(loss=[power_spectrum_loss], optimizer=opt, loss_weights=None) # =['mean_squared_error', 'mae']
     return model
     
 # load and prepare training images
