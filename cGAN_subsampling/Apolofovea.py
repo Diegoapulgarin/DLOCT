55d996b95b3704980f54e7d47a33090f0f2435da
Diegoapulgarin <104539064+Diegoapulgarin@users.noreply.github.com>
2023-05-08 09:30:05 -0500

    commit test

--- cGAN_subsampling/Apolofovea.py
+++ cGAN_subsampling/Apolofovea.py
@@ -4,3 +4,4 @@ import numpy as np
 path = r'D:\DLOCT\TomogramsDataAcquisition\Fovea\No_motion_corrected' # own pc
 tomData1 = np.load(path + '\\tomDataOver_586_896_0%3A896_pol1.npy')
 tomData2 = np.load(path + '\\tomDataOver_586_896_64%3A960_pol1.npy')
+#test
