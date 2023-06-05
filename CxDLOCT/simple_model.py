2ae9e1b761a170ac73b261d20486c059f7752f0f
Diegoapulgarin <104539064+Diegoapulgarin@users.noreply.github.com>
2023-06-04 19:34:13 -0500

    minor changes

--- CxDLOCT/simple_model.py
+++ CxDLOCT/simple_model.py
@@ -1,7 +1,8 @@
+#%%
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
+#%%
 # Tamaño de la imagen 3D
 width, height, depth = 100, 100, 100
 
@@ -52,8 +53,37 @@ threshold = 55
 ax.scatter(x[image < threshold], y[image < threshold], z[image < threshold], c=image[image < threshold], cmap='gray', s=5)
 
 plt.show()
+#%%
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+# Coordenada específica para las vistas ortogonales
+posicion = 20
 
+# Obtener las vistas ortogonales en la coordenada específica
+vista_xy = image[:, :, posicion]  # Vista XY (proyección en el plano Z)
+vista_xz = image[:, posicion, :]  # Vista XZ (proyección en el plano Y)
+vista_yz = image[posicion, :, :]  # Vista YZ (proyección en el plano X)
 
+fig = make_subplots(rows=1, cols=3, subplot_titles=('Vista XY', 'Vista XZ', 'Vista YZ'))
+fig.add_trace(go.Heatmap(z=vista_xy, colorscale='Viridis'), row=1, col=1)
+fig.add_trace(go.Heatmap(z=vista_xz, colorscale='Viridis'), row=1, col=2)
+fig.add_trace(go.Heatmap(z=vista_yz, colorscale='Viridis'), row=1, col=3)
+
+# Personalizar el diseño del gráfico
+fig.update_layout(
+    title='Vistas Ortogonales 2D',
+    height=600,
+    width=1000
+)
+
+# Mostrar el gráfico
+fig.show()
+#%%
+fig2 = px.line(phase_difference[50,50,:])
+fig2.show()
+
+#%%
 
 
 
