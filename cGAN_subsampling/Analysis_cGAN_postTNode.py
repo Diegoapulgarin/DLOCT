8005cfdf94490e55b3b82aa200655d27e94bf535
Diegoapulgarin <dapulgaris@eafit.edu.co>
2023-07-23 17:13:44 -0500

    nimor changes

--- cGAN_subsampling/Analysis_cGAN_postTNode.py
+++ cGAN_subsampling/Analysis_cGAN_postTNode.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 from Deep_Utils import create_and_save_subplot, tiff_3Dsave
 #%%
-path = r'D:\DLOCT\TomogramsDataAcquisition\Fovea\No_motion_corrected\Final para analizar\z=(1..586)_x=(155..741)_y=(187..773)-nSpec=cGAN'
+path = r'D:\DLOCT\TomogramsDataAcquisition\Fovea\Motion_corrected\Final'
 file = 'TNodeIntFlattenRPE.bin'
 nXBin = 587
 nYBin = 587
@@ -19,8 +19,8 @@ for filename in os.listdir(os.getcwd()):
 tomint = np.array(tomint) # index 0 = cGAN, index 1 = original
 #%%
 thisbscan=256
-plot_cGAN = 10*np.log10(abs(tomint[0,:,:,thisbscan])**2)
-plot_orig = 10*np.log10(abs(tomint[1,:,:,thisbscan])**2)
+plot_cGAN = 10*np.log10(abs(tomint[1,:,:,thisbscan])**2)
+plot_orig = 10*np.log10(abs(tomint[0,:,:,thisbscan])**2)
 zmax=250
 zmin = 160
 file = 'ZX_256'
@@ -32,9 +32,9 @@ create_and_save_subplot(plot_cGAN,plot_orig,
                         ,zmax=zmax,zmin=zmin,
                         file_name=file)
 #%%
-thisbscan=160
-plot_cGAN = 10*np.log10(abs(tomint[2,thisbscan,:,:])**2)
-plot_orig = 10*np.log10(abs(tomint[1,thisbscan,:,:])**2)
+thisbscan=170
+plot_cGAN = 10*np.log10(abs(tomint[1,thisbscan,:,:])**2)
+plot_orig = 10*np.log10(abs(tomint[0,thisbscan,:,:])**2)
 zmax=250
 zmin = 160
 file = 'XY_256'
@@ -47,8 +47,8 @@ create_and_save_subplot(plot_cGAN,plot_orig,
                         file_name=file)
 #%%
 thisbscan=160
-plot_cGAN = 10*np.log10(abs(tomint[0,:,thisbscan,:])**2)
-plot_orig = 10*np.log10(abs(tomint[1,:,thisbscan,:])**2)
+plot_cGAN = 10*np.log10(abs(tomint[1,:,thisbscan,:])**2)
+plot_orig = 10*np.log10(abs(tomint[0,:,thisbscan,:])**2)
 zmax=250
 zmin = 160
 file = 'ZY_256'
@@ -60,7 +60,28 @@ create_and_save_subplot(plot_cGAN,plot_orig,
                         ,zmax=zmax,zmin=zmin,
                         file_name=file)
 #%%
+#%%
+original_array = np.transpose(tomint, (1, 2, 3, 0))
+
+# Redimensionamos los volúmenes para que tengan tres dimensiones (x, y, 2z)
+volume1 = original_array[:,:,:,0].reshape((586, 587, -1))
+volume2 = original_array[:,:,:,1].reshape((586, 587, -1))
+
+# Concatenamos los volúmenes a lo largo del eje Y
+compare = np.concatenate((volume1, volume2), axis=1)
+compare = np.transpose(compare,(2,0,1))
+del volume1, volume2
+#%%
+import plotly.express as px
+#%%
+plot_test = 10*np.log10(abs(compare[240,:,:])**2)
+fig = px.imshow(plot_test,zmax=250,zmin=160,color_continuous_scale='gray')
+fig.show()
+#%%
 filename = '\cGANtomintTNode.tiff'
-tiff_3Dsave(tomint[0,:,:,:],output+filename)
+tiff_3Dsave(10*np.log10(tomint[1,:,:,:]),output+filename)
 filename = '\OriginaltomintTNode.tiff'
-tiff_3Dsave(tomint[1,:,:,:],output+filename)
+tiff_3Dsave(10*np.log10(tomint[0,:,:,:]),output+filename)
+#%%
+filename = '\compare_original_cGAN.tiff'
+tiff_3Dsave(10*np.log10((compare)),output+filename)
