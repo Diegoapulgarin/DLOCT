b536c2df15b6796f285da7c74066d288e78cb84e
Diegoapulgarin <dapulgaris@eafit.edu.co>
2023-10-24 14:13:27 -0500

    minor changes

--- CxDLOCT/PreProcessingCxDLOCT.py
+++ CxDLOCT/PreProcessingCxDLOCT.py
@@ -14,14 +14,8 @@ import plotly.subplots as sp
 from scipy.signal import hilbert
 from plotly.subplots import make_subplots
 from scipy.signal import butter, filtfilt
+from PyEMD import EMD
 #%%
-
-def low_pass_filter(data, cutoff, fs, order=5):
-    nyq = 0.5 * fs  # Frecuencia de Nyquist
-    normal_cutoff = cutoff / nyq
-    b, a = butter(order, normal_cutoff, btype='low', analog=False)
-    y = filtfilt(b, a, data)
-    return y
 
 path = r'C:\Users\USER\Documents\GitHub\Simulated_Data_Complex'
 os.chdir(path)
@@ -43,19 +37,25 @@ del fringes1, fringes_slice
 thisfringes = 2
 fringesTest = fringes[thisfringes,:,:,:]
 real_fringe = np.abs(fringesTest) * np.cos(np.angle(fringesTest))
-fringe_filtered = low_pass_filter(real_fringe, cutoff=40, fs=500)  # Ajusta los valores según tus datos
+emd = EMD()
+IMFs = emd(real_fringe[:,1,1])
 
+# Elije una IMF específica, por ejemplo la primera:
+imf_selected = IMFs[0]
+combined_IMF = np.sum(IMFs[:6], axis=0)
+# Obtener la señal analítica
+analytic_signal = hilbert(combined_IMF)
 
-analytic_signal = hilbert(fringe_filtered)
-
+# Calcular la envolvente
 envelope = np.abs(analytic_signal)
-#%%
+
+
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(y=(envelope[:,1,1]), mode='lines', name='+ envelope',
+fig.add_trace(go.Scatter(y=(envelope), mode='lines', name='+ envelope',
                          line=dict(dash='dash')))
 fig.add_trace(go.Scatter(y=(real_fringe[:,1,1]), mode='lines', name='fringe'))
-fig.add_trace(go.Scatter(y=(-envelope[:,1,1]), mode='lines', name='- envelope',
+fig.add_trace(go.Scatter(y=(-envelope), mode='lines', name='- envelope',
                          line=dict(dash='dash')))
 fig.show()
 
@@ -63,13 +63,11 @@ fig.show()
 thisfringes = 2
 fringesTest = fringes[thisfringes,:,:,:]
 real_fringe = np.abs(fringesTest) * np.cos(np.angle(fringesTest))
-analytic_signal = hilbert(real_fringe)
-envelope = np.abs(analytic_signal)
 def moving_average(data, window_size):
     return np.convolve(data, np.ones(window_size)/window_size, mode='valid')
 
 window_size = 20  # Puedes ajustar este valor según tus necesidades
-smoothed_envelope = moving_average(envelope[:,1,1], window_size)
+smoothed_envelope = moving_average(envelope, window_size)
 
 second_derivative_smoothed = np.diff(smoothed_envelope, n=2)
 zero_crossings_smoothed = np.where(np.diff(np.sign(second_derivative_smoothed)))[0]
@@ -83,25 +81,6 @@ for i in range(1, len(zero_crossings_smoothed)):
 
 consolidated_crossings = np.array(consolidated_crossings)
 
-def segment_signal(signal, crossings):
-    fragments = []
-    for i in range(0, len(crossings) - 1, 2):  # Tomamos los puntos de inflexión en pares
-        fragment = np.zeros_like(signal)
-        start, end = crossings[i], crossings[i + 1]
-        
-        # Copia la señal del segmento en el fragmento
-        fragment[start:end] = signal[start:end]
-        
-        # Añade el fragmento a la lista de fragmentos
-        fragments.append(fragment)
-        
-    return np.array(fragments)
-
-# Segmentamos la señal
-segmented_fringes = segment_signal(real_fringe[:,1,1], consolidated_crossings)
-
-# Verificamos las dimensiones de los segmentos obtenidos
-print(segmented_fringes.shape)
 #%%
 def segment_signal_without_padding(signal, crossings):
     fragments = []
@@ -151,27 +130,27 @@ complex_signal = real_fringe[:,1,1] * np.exp(1j * accumulated_phase)
 #%% comprobación fft
 fft_fringetest = np.fft.fftshift(np.fft.fft(fringesTest[:,1,1]))
 fft_complexsignal = np.fft.fftshift(np.fft.fft(complex_signal))
-fft_realfringe = np.fft.fftshift(np.fft.fft(real_fringe[:,1,1]+1j*np.imag(fringesTest[:,1,1])))
+fft_realfringe = np.fft.fftshift(np.fft.fft(real_fringe[:,1,1]))
 
 
 # Crear un subplot con 2 filas y 1 columna
 fig = make_subplots(rows=3, cols=1)
 
 # Agregar la primera traza al primer subplot
-fig.add_trace(go.Scatter(y=abs(fft_fringetest), mode='lines', name='fft original'), row=1, col=1)
+fig.add_trace(go.Scatter(y=abs(fft_fringetest), mode='lines', name='fft original complex_signal'), row=1, col=1)
 
 # Agregar la segunda traza al segundo subplot
-fig.add_trace(go.Scatter(y=abs(fft_complexsignal), mode='lines', name='fft estimated'), row=2, col=1)
+fig.add_trace(go.Scatter(y=abs(fft_complexsignal), mode='lines', name='fft estimated complex_signal'), row=2, col=1)
 
-fig.add_trace(go.Scatter(y=abs(fft_realfringe), mode='lines', name='fft complex conjugate'), row=3, col=1)
+fig.add_trace(go.Scatter(y=abs(fft_realfringe), mode='lines', name='fft real signal'), row=3, col=1)
 
 # Mostrar la figura
 fig.show()
 
 
 fig=go.Figure()
-fig.add_trace(go.Scatter(y=(np.imag(fringesTest[:,1,1])), mode='lines', name='original'))
-fig.add_trace(go.Scatter(y=(np.imag(complex_signal)), mode='lines', name='estimated'))
+fig.add_trace(go.Scatter(y=(np.imag(fringesTest[:,1,1])), mode='lines', name='imag part original'))
+fig.add_trace(go.Scatter(y=(np.imag(complex_signal)), mode='lines', name='imag estimated'))
 fig.show()
 
 
