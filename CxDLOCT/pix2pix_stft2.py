145febe6b9231c8bd5b5862686474f13edc263e2
Diegoapulgarin <dapulgaris@eafit.edu.co>
2024-02-15 10:29:16 -0500

    pix2pix ready for train!

--- CxDLOCT/pix2pix_stft2.py
+++ CxDLOCT/pix2pix_stft2.py
@@ -205,5 +205,291 @@ _,stft_volume_tomograms = compute_separate_stft_for_volume(all_tomograms[0:2286,
 _,stft_volume_targets = compute_separate_stft_for_volume(all_targets[0:2286,:,:],fs, nperseg, noverlap)
 # _, _, Zxx_complex = stft(fringesreal[0:2286,512,0], fs=fs, nperseg=nperseg, noverlap=noverlap)
 #%%
-plt.imshow((abs(stft_volume_tomograms[:,:,512,0])))
+# plt.imshow((abs(stft_volume_tomograms[:,:,512,0])))
+dim = np.shape(stft_volume_tomograms)
+stft_tomograms = np.reshape(stft_volume_tomograms,(dim[0],dim[1],(dim[2]*dim[3])))
+stft_targets = np.reshape(stft_volume_targets,(dim[0],dim[1],(dim[2]*dim[3])))
 #%%
+def basicNormalize(volume):
+    n_stft = volume.shape[2]
+    mean_vals = np.zeros(n_stft)
+    std_vals = np.zeros(n_stft)
+    volume_normalized = np.zeros_like(volume)
+    for i in range(n_stft):
+        mean_vals[i] = np.mean(volume[:, :, i])
+        std_vals[i] = np.std(volume[:, :, i])  
+        volume_normalized[:, :, i] = (volume[:, :, i] - mean_vals[i]) / std_vals[i]
+    return volume_normalized,mean_vals,std_vals
+amp_tomograms = abs(stft_tomograms)
+amp_targets = abs(stft_targets)
+phase_tomograms = np.angle(stft_tomograms)
+phase_targets = np.angle(stft_targets)
+amp_normalized_tomograms,mean_tomograms,std_tomograms = basicNormalize(amp_tomograms)
+amp_normalized_targets,mean_targets,std_targets = basicNormalize(amp_targets)
+del amp_tomograms, amp_targets, fringescc,fringesreal
+#%%
+dim=np.shape(amp_normalized_targets)
+X = np.zeros((dim[0],dim[1],dim[2],2))
+X[:,:,:,0] = np.real(amp_normalized_tomograms*np.exp(1j*phase_tomograms))
+X[:,:,:,1] = np.imag(amp_normalized_tomograms*np.exp(1j*phase_tomograms))
+Y = np.zeros((dim[0],dim[1],dim[2],2))
+Y[:,:,:,0] = np.real(amp_normalized_targets*np.exp(1j*phase_targets))
+Y[:,:,:,1] = np.imag(amp_normalized_targets*np.exp(1j*phase_targets))
+X = np.transpose(X,(2,0,1,3))
+Y = np.transpose(Y,(2,0,1,3))
+#%%
+
+
+# define the discriminator model
+def define_discriminator(image_shape):
+    # weight initialization
+    init = RandomNormal(stddev=0.02)
+    # source image input
+    in_src_image = Input(shape=image_shape)
+    # target image input
+    in_target_image = Input(shape=image_shape)
+    # concatenate images channel-wise
+    merged = Concatenate()([in_src_image, in_target_image])
+    # C64
+    d = Conv2D(64, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(merged)
+    d = ReLU()(d)#LeakyReLU(alpha=0.2)(d) #LeakyReLU
+    
+    # C128
+    d = Conv2D(128, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = ReLU()(d)
+    # C256
+    d = Conv2D(256, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = ReLU()(d)
+    # C512
+    d = Conv2D(512, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = ReLU()(d)
+    # second last output layer
+    d = Conv2D(512, (4,4), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = ReLU()(d)
+    # patch output
+    d = Conv2D(1, (4,4), padding='same', kernel_initializer=init)(d)
+    patch_out = Activation('relu')(d)
+    # define model
+    model = Model([in_src_image, in_target_image], patch_out)
+    # compile model
+    opt = Adam(learning_rate=0.0000001)#Adam , beta_1=0.5 , RMSprop
+    model.compile(loss=['binary_crossentropy'], optimizer=opt, loss_weights=None)#'binary_crossentropy'
+    
+    return model
+    
+# define an encoder block
+def define_encoder_block(layer_in, n_filters, batchnorm=True):
+    # weight initialization
+    init = RandomNormal(stddev=0.02)
+    # add downsampling layer
+    g = Conv2D(n_filters, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(layer_in)
+    # conditionally add batch normalization
+    if batchnorm:
+        g = BatchNormalization()(g, training=True)
+    # leaky relu activation
+    g = LeakyReLU(alpha=0.2)(g)
+    return g
+    
+# define a decoder block
+def decoder_block(layer_in, skip_in, n_filters, dropout=True):
+    # weight initialization
+    init = RandomNormal(stddev=0.02)
+    # add upsampling layer
+    g = Conv2DTranspose(n_filters, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(layer_in)
+    # add batch normalization
+    g = BatchNormalization()(g, training=True)
+    # conditionally add dropout
+    if dropout:
+        g = Dropout(0.5)(g, training=True)
+    # merge with skip connection
+    g = Concatenate()([g, skip_in])
+    # relu activation
+    g = Activation('relu')(g)
+    return g
+
+# define the standalone generator model
+def define_generator(image_shape):
+    # weight initialization
+    init = RandomNormal(stddev=0.02)
+    # image input
+    in_image = Input(shape=image_shape)
+    # encoder model -> Secuencia de Convolución y activación -> Encoder ascendente
+    e1 = define_encoder_block(in_image, 64, batchnorm=False)
+    e2 = define_encoder_block(e1, 128)
+    e3 = define_encoder_block(e2, 256)
+    e4 = define_encoder_block(e3, 512)
+    e5 = define_encoder_block(e4, 512)
+    e6 = define_encoder_block(e5, 512)
+    #e7 = define_encoder_block(e6, 512)
+    # bottleneck, no batch norm and relu
+    b = Conv2D(512, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(e6)
+    b = Activation('relu')(b)
+    # decoder model -> Secuencia de Convolución y activación -> Decoder descendente
+    d1 = decoder_block(b, e6, 512)
+    d2 = decoder_block(d1, e5, 512)
+    d3 = decoder_block(d2, e4, 512)
+    d4 = decoder_block(d3, e3, 256, dropout=False)
+    d5 = decoder_block(d4, e2, 128, dropout=False)
+    d6 = decoder_block(d5, e1, 64, dropout=False)
+    #d7 = decoder_block(d6, e1, 64, dropout=False)
+    # output
+    g = Conv2DTranspose(image_shape[2], (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d6)
+    out_image = Activation('relu')(g) #'tanh'
+    # define model
+    model = Model(in_image, out_image)
+    return model
+    
+# define the combined generator and discriminator model, for updating the generator
+def define_gan(g_model, d_model, image_shape):
+    # make weights in the discriminator not trainable
+    d_model.trainable = False
+    # define the source image
+    in_src = Input(shape=image_shape)
+    # connect the source image to the generator input
+    gen_out = g_model(in_src)
+    # connect the source input and generator output to the discriminator input
+    dis_out = d_model([in_src, gen_out])
+    # src image as input, generated image and classification output
+    model = Model(in_src, [dis_out, gen_out])
+    # compile model
+    opt = RMSprop(learning_rate=0.0002)
+    model.compile(loss=['mean_squared_error', 'mae'], optimizer=opt, loss_weights=None) # =['mean_squared_error', 'mae']
+    return model
+    
+# load and prepare training images
+def load_real_samples(filename):
+    # load the compressed arrays
+    data = load(filename)
+    # unpack the arrays
+    X1, X2 = data['arr_0'], data['arr_1']
+    # scale from [0,255] to [-1,1]
+    X1 = (X1 - 127.5) / 127.5
+    X2 = (X2 - 127.5) / 127.5
+    return [X1, X2]
+    
+# select a batch of random samples, returns images and target
+def generate_real_samples(dataset, n_samples, patch_shape):
+    # unpack dataset
+    trainA, trainB = dataset
+    # choose random instances
+    ix = randint(0, trainA.shape[0], n_samples)
+    # retrieve selected images
+    X1, X2 = trainA[ix], trainB[ix]
+    # slice max and min
+    # generate âœ¬realâœ¬ class labels (1)
+    y = ones((n_samples, patch_shape, patch_shape, 1))
+    return [X1, X2], y 
+    
+# generate a batch of images, returns images and targets
+def generate_fake_samples(g_model, samples, patch_shape):
+    # generate fake instance
+    X = g_model.predict(samples)
+    # create âœ¬fakeâœ¬ class labels (0)
+    y = zeros((len(X), patch_shape, patch_shape, 1))
+    return X, y
+
+# generate samples and save as a plot and save the model
+def summarize_performance(step, g_model, dataset, n_samples=3):
+    # select a sample of input images
+    [X_realA, X_realB], _ = generate_real_samples(dataset, n_samples,1)
+    # generate a batch of fake samples
+    X_fakeB, _ = generate_fake_samples(g_model, X_realA, 1)
+    # plot real source images
+    for i in range(n_samples):
+        spectogram = X_realA[i]
+        plot0 = spectogram[:,:,0]+1j*spectogram[:,:,1]
+        pyplot.subplot(3, n_samples, 1 + i)
+        pyplot.axis('off')
+        pyplot.imshow(abs(plot0), cmap='gray')
+    # plot generated target image
+    for i in range(n_samples):
+        spectogram = X_fakeB[i]
+        plot1 = spectogram[:,:,0]+1j*spectogram[:,:,1]
+        pyplot.subplot(3, n_samples, 1 + n_samples + i)
+        pyplot.axis('off')
+        pyplot.imshow(abs(plot1), cmap='gray')
+    # plot real target image
+    for i in range(n_samples):
+        spectogram = X_realB[i]
+        plot2 = spectogram[:,:,0]+1j*spectogram[:,:,1]
+        pyplot.subplot(3, n_samples, 1 + n_samples*2 + i)
+        pyplot.axis('off')
+        pyplot.imshow(abs(plot2), cmap='gray')
+    # save plot to file
+    filename1 = 'C:/Users/USER/Documents/GitHub/plot_%06d.png' % (step+1)
+    pyplot.savefig(filename1)
+    pyplot.close()
+    # save the generator model
+    filename2 = 'C:/Users/USER/Documents/GitHub/model_%06d.h5' % (step+1)
+    g_model.save(filename2)
+    print('>Saved: %s and %s' % (filename1, filename2))
+
+# train pix2pix models
+def train(d_model, g_model, gan_model, dataset, n_epochs, n_batch=1):
+    # determine the output square shape of the discriminator
+    n_patch = d_model.output_shape[1]
+    # unpack dataset
+    trainA, trainB = dataset
+    # calculate the number of batches per training epoch
+    bat_per_epo = int(len(trainA) / n_batch)
+    # calculate the number of training iterations
+    n_steps = bat_per_epo * n_epochs
+    # manually enumerate epochs
+    d_loss1_val = []
+    d_loss2_val = []
+    g_loss_val  = []
+    n_steps_val = []
+    for i in range(n_steps):
+        # select a batch of real samples
+        [X_realA, X_realB], y_real = generate_real_samples(dataset, n_batch, n_patch)
+        # generate a batch of fake samples
+        X_fakeB, y_fake = generate_fake_samples(g_model, X_realA, n_patch)
+        # update discriminator for real samples
+        d_loss1 = d_model.train_on_batch([X_realA, X_realB], y_real)
+        # update discriminator for generated samples
+        d_loss2 = d_model.train_on_batch([X_realA, X_fakeB], y_fake)
+        # update the generator
+        g_loss, _, _ = gan_model.train_on_batch(X_realA, [y_real, X_realB])
+        # summarize performance
+        print('>%d/%d, d1[%.3f] d2[%.3f] g[%.3f]' % (i+1,n_steps, d_loss1, d_loss2, g_loss))
+        d_loss1_val.append(d_loss1)
+        d_loss2_val.append(d_loss2)
+        g_loss_val.append(g_loss)
+        
+        #summarize model performance
+        if (i+1) % (bat_per_epo * 1) == 0:
+            summarize_performance(i, g_model, dataset)
+            # calculate mean of losses in epoch
+            d_loss1_mean = np.array(d_loss1_val).mean()
+            d_loss2_mean = np.array(d_loss2_val).mean()
+            g_loss_mean = np.array(g_loss_val).mean()
+            # concatenate losses values
+            d_loss1_epoch.append(d_loss1_mean)
+            d_loss2_epoch.append(d_loss2_mean)
+            g_loss_epoch.append(g_loss_mean)
+            n_steps_epoch.append(int(i/bat_per_epo))
+            # reset losses values
+            d_loss1_val = []
+            d_loss2_val = []
+            g_loss_val  = []
+
+#%%
+dataset = [X,Y]
+shape = np.shape(X)
+image_shape = (shape[1],shape[2],shape[3])
+#%%
+# define the models
+d_model = define_discriminator(image_shape)
+g_model = define_generator(image_shape)
+gan_model = define_gan(g_model, d_model, image_shape)
+d_loss1_epoch = []
+d_loss2_epoch = []
+g_loss_epoch  = []
+n_steps_epoch = []
+n_epochs = 10
+#%%
+train(d_model, g_model, gan_model, dataset,n_epochs)
