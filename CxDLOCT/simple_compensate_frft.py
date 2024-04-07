6812c5cc573b87e7a6bbb130c9b6bc020e21fe11
Diegoapulgarin <dapulgaris@eafit.edu.co>
2024-04-07 03:48:29 -0500

    minor changes simple compensate

--- CxDLOCT/simple_compensate_frft.py
+++ CxDLOCT/simple_compensate_frft.py
@@ -52,29 +52,29 @@ print( 'Mean GPU time = %f μs'%mean( gputime ) )
 n = int(nSnapshots/2)
 phasefrft = np.angle(results[n])
 peaks, _ = find_peaks(phasefrft, height=0)
-fig,ax = plt.subplots(3,1)
-ax[1].plot(abs(results[n]))
-ax[2].plot(phasefrft)
-ax[0].plot(abs(fftscv))
-plt.plot()
+# fig,ax = plt.subplots(3,1)
+# ax[1].plot(abs(results[n]))
+# ax[2].plot(phasefrft)
+# ax[0].plot(abs(fftscv))
+# plt.plot()
 #%%
 # %matplotlib auto
 z = 150
 frftarray = np.array(results)
 phasefrft = np.angle(frftarray)
 
-fig = make_subplots(rows=3, cols=1)
+fig = make_subplots(rows=1, cols=3)
 fig.add_trace(
     go.Scatter(y=np.abs(fftscv), mode='lines', name='target signal'),
     row=1, col=1
 )
 fig.add_trace(
     go.Scatter(y=np.abs(frftarray[n, :]), mode='lines', name='frft transform with alpha aprox 1'),
-    row=2, col=1
+    row=1, col=2
 )
 fig.add_trace(
     go.Scatter(y=np.abs(np.unwrap(phasefrft[n, :])), mode='lines', name='unwrapped phase'),
-    row=3, col=1
+    row=1, col=3
 )
 fig.update_layout(
     title_text="Análisis de Señales",
@@ -82,8 +82,46 @@ fig.update_layout(
     showlegend=False
 )
 fig.update_xaxes(title_text="Señal objetivo", row=1, col=1)
-fig.update_xaxes(title_text="Transformada FRFT con alpha ~1", row=2, col=1)
-fig.update_xaxes(title_text="Fase desenvuelta", row=3, col=1)
+fig.update_xaxes(title_text="Transformada FRFT con alpha ~1", row=1, col=2)
+fig.update_xaxes(title_text="Fase desenvuelta", row=1, col=3)
 fig.show()
-#%%
 # plt.imshow(abs(frftarray),cmap='viridis')
+#%%
+n = int(nSnapshots / 2)
+alpha_central = frftarray[n, :]
+phase_central = phasefrft[n, :]
+unwrapped_phase = abs(np.unwrap(phase_central))
+peaks, _ = find_peaks(np.abs(fftsrv),height=0.5)  # review criteria
+range_to_search = len(fftsrv) // 2 
+fftsrv_compensated = np.copy(fftsrv)
+for peak in peaks:
+    is_real_peak = unwrapped_phase[peak] > unwrapped_phase[range_to_search * 2 - peak]
+    peak_to_attenuate = peak if not is_real_peak else range_to_search * 2 - peak
+    N = len(fftsrv)
+    if peak_to_attenuate < N / 2:
+        frequency = (peak_to_attenuate / N) * fs
+    else:
+        frequency = ((peak_to_attenuate - N) / N) * fs
+    A = np.abs(fftsrv[peak_to_attenuate])
+    attenuation_signal = fft(A * np.exp(-1j * (2 * np.pi * frequency * t)))
+    fftsrv_compensated -= attenuation_signal
+signal_compensated = np.fft.ifft(fftsrv_compensated)
+#%%
+fig = make_subplots(rows=1, cols=2)
+fig.add_trace(
+    go.Scatter(y=np.abs(fftscv), mode='lines', name='target signal'),
+    row=1, col=1
+)
+fig.add_trace(
+    go.Scatter(y=np.abs(fftsrv_compensated), mode='lines', name='compensated signal'),
+    row=1, col=2
+)
+fig.update_layout(
+    title_text="Results",
+    height=900,  # Ajusta la altura si es necesario
+    showlegend=False
+)
+fig.update_xaxes(title_text="Señal objetivo", row=1, col=1)
+fig.update_xaxes(title_text="compensated signal", row=1, col=2)
+
+fig.show()
